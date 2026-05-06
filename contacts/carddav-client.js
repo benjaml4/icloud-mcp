32e994fc82519f9047178e7dfd531f2d9c897564
Dan Bennett <dan@danbennett.me>
2026-05-06 22:10:47 +0200

    Search the full CardDAV contact set

--- contacts/carddav-client.js
+++ contacts/carddav-client.js
@@ -204,7 +204,7 @@ async function listContacts(count = 25) {
  * Search contacts
  */
 async function searchContacts(query, count = 25) {
-  const allContacts = await listContacts(count * 2);
+  const allContacts = await listContacts(config.DEFAULTS.MAX_RESULTS);
   const lowerQuery = query.toLowerCase();
 
   const matches = allContacts.filter(contact => {
